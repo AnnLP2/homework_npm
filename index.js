e3b5479519d30ea466b41eaa05faa548049e2e6e
AnnLP2 <ann.vitalina@gmail.com>
2023-03-21 00:50:46 +0200

    project files checked out by eslint

---
+++ index.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+add(5, 6);
+function subtract(a, b) {
+  return a - b;
+}
+subtract(10, 15);
+function multiply(a, b) {
+  return a * b;
+}
+multiply(2, 7);
