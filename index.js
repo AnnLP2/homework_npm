0e73ee5f54f86d07b30cc4cb806220179d9a9ac2
AnnLP2 <ann.vitalina@gmail.com>
2023-03-21 02:46:56 +0200

    project files are checked

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function add(a, b) {
   return a + b;
 }
+
 add(5, 6);
+
 function subtract(a, b) {
   return a - b;
 }
+
 subtract(10, 15);
+
 function multiply(a, b) {
   return a * b;
 }
+
 multiply(2, 7);
